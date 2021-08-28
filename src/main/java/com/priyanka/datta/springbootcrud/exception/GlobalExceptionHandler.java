dba11a27d25dfc2a3a1fec238cf2e4812189e44d
Priyanka Datta <priyankadatta1696@gmail.com>
2021-08-28 17:23:32 +0530

    Exception handling

--- src/main/java/com/priyanka/datta/springbootcrud/exception/GlobalExceptionHandler.java
+++ src/main/java/com/priyanka/datta/springbootcrud/exception/GlobalExceptionHandler.java
@@ -4,9 +4,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EmployeeNotFoundException.class)
     public ResponseEntity<Object> handleEmployeeNotFoundException(EmployeeNotFoundException e){
